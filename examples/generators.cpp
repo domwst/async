d29d0378692ec0cbfaa2eaa12069e9a17bde95e6
Oleg Shatov <8730199@gmail.com>
2022-04-18 13:09:53 +0300

    Enchanced generator examples

--- examples/generators.cpp
+++ examples/generators.cpp
@@ -5,6 +5,22 @@
 
 using async::coroutine::Generator;
 
+void Simple() {
+  Generator<size_t> naturals([]() {
+    size_t n = 0;
+    async::util::Defer scope_guard([]() {
+      std::cout << "Generator finished" << std::endl;
+    });
+    while (Generator<size_t>::Yield(n++)) {}
+  });
+
+  for (size_t i = 0; i < 10; ++i) {
+    naturals.Get();
+  }
+  naturals.Cancel();  // Should print "Generator finished" before "Test finished"
+  std::cout << "Test finished" << std::endl;
+}
+
 void Nested() {
   Generator<size_t> ten_squares([]() {
     Generator<size_t> naturals([]() {
@@ -32,7 +48,7 @@ void MagicStop() {
   Generator<size_t> naturals([]() {
     size_t n = 0;
     async::util::Defer scope_guard([]() {
-      std::cout << "Finished" << std::endl;
+      std::cout << "Generator finished" << std::endl;
     });
     while (true) {
       size_t val = n++;
@@ -46,8 +62,8 @@ void MagicStop() {
     }
     std::cout << *val << std::endl;
   }
-  naturals.Cancel();
-  // Should print "Finished"
+  naturals.Cancel();  // Should print "Generator finished" before "Test finished"
+  std::cout << "Test finished" << std::endl;
 }
 
 void MoveOnly() {
