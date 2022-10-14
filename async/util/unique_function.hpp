73f86a6a97d4925a0544ea2457a0c1a0fa56a087
Oleg Shatov <8730199@gmail.com>
2022-10-14 13:02:44 +0600

    Added UniqueFunction

---
+++ async/util/unique_function.hpp
@@ -0,0 +1,15 @@
+#include <async/util/function2/function2.hpp>
+
+namespace async {
+
+template<class Signature>
+using UniqueFunction = fu2::function_base<
+  /* IsOwning= */                 true,
+  /* IsCopyable= */               false,
+  /* Capacity= */                 fu2::capacity_fixed<64U>,
+  /* IsThrowing= */               true,
+  /* HasStrongExceptGuarantee= */ false,
+  /* Signatures= */               Signature
+>;
+
+}  // namespace async
